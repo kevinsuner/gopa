9896394f6b2eb188bf9d8565b34e465ec342534c
Kevin Suñer <ksuner@pm.me>
2024-05-10 00:21:12 +0200

    feat: renamed file, added Uncompress to determine which fn to use based on the os, added unzip fn

---
+++ uncompress.go
@@ -0,0 +1,127 @@
+/*
+* SPDX-License-Identifier: GPL-3.0-only
+* Copyright (C) 2024 Kevin Suñer <ksuner@pm.me>
+ */
+
+package main
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"bytes"
+	"compress/gzip"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Uncompress reads a stream of data, and determines how to uncompress it based
+// on the operating system passed in the function arguments.
+func Uncompress(reader io.ReadCloser, target, os string) error {
+    if os == "windows" {
+        return unzip(reader, target)
+    }
+
+    return untar(reader, target)
+}
+
+// Extracts the data from a .zip file to the specified target directory.
+func unzip(reader io.ReadCloser, target string) error {
+    var buf *bytes.Buffer
+    size, err := io.Copy(buf, reader)
+    if err != nil {
+        return err
+    }
+
+    bytesReader := bytes.NewReader(buf.Bytes())
+    zipReader, err := zip.NewReader(bytesReader, size)
+    if err != nil {
+        return err
+    }
+
+    for _, file := range zipReader.File {
+        path := filepath.Clean(filepath.Join(target, file.Name))
+        if !strings.HasPrefix(path, target) {
+            return errors.New("invalid file path")
+        }
+    
+        info := file.FileInfo()
+        if info.IsDir() {
+            if err := os.MkdirAll(path, info.Mode()); err != nil { return err }
+            continue
+        }
+
+        dstFile, err := os.OpenFile(
+            path, os.O_CREATE | os.O_TRUNC | os.O_RDWR, info.Mode())
+        if err != nil {
+            return err
+        }
+
+        defer func() error {
+            return dstFile.Close()
+        }()
+
+        srcFile, err := file.Open()
+        if err != nil {
+            return err
+        }
+
+        defer func() error {
+            return srcFile.Close()
+        }()
+
+        if _, err := io.Copy(dstFile, srcFile); err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
+
+// Extracts the data from a .tar.gz file to the specified target directory.
+func untar(reader io.ReadCloser, target string) error {
+    gzipReader, err := gzip.NewReader(reader)
+    if err != nil {
+        return err
+    }
+    
+    defer func() error {
+        return gzipReader.Close()
+    }()
+
+    tarReader := tar.NewReader(gzipReader)
+    for {
+        header, err := tarReader.Next()
+        if err != nil {
+            if err == io.EOF { break }
+            return err
+        }
+
+        path := filepath.Join(target, header.Name)
+        info := header.FileInfo()
+        if info.IsDir() {
+            if err := os.MkdirAll(path, info.Mode()); err != nil { return err }
+            continue
+        }
+
+        file, err := os.OpenFile(
+            path, os.O_CREATE | os.O_TRUNC | os.O_RDWR, info.Mode())
+        if err != nil {
+            return err
+        }
+        
+        defer func() error {
+            return file.Close()
+        }()
+
+        _, err = io.Copy(file, tarReader)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
+
