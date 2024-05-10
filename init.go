9abf4c96f58cc23c83008679bbb089df30d02485
Kevin Suñer <ksuner@pm.me>
2024-05-11 00:40:56 +0200

    refactor: passing path in fn args, renamed variables

--- init.go
+++ init.go
@@ -26,25 +26,21 @@ var goURL = "https://go.dev/dl/go1.22.3.%s-%s.%s"
 
 // Init creates the required folders and files for the project to work,
 // and downloads the latest Go version based on the host's operating system.
-func Init() (string, error) {
-    home, err := os.UserHomeDir()
-    if err != nil {
-        return "", err
-    }
+func Init(path string) (string, error) {
+    basepath := filepath.Join(path, APP_DIR)
 
-    rootDir := filepath.Join(home, APP_DIR)
-    err = os.Mkdir(rootDir, os.ModePerm)
+    err := os.Mkdir(basepath, os.ModePerm)
     if err != nil && !errors.Is(err, fs.ErrExist) {
         return "", err
     }
 
-    file, err := os.Create(filepath.Join(rootDir, LOG_FILE))
+    file, err := os.Create(filepath.Join(basepath, LOG_FILE))
     if err != nil {
         return "", err
     }
     defer file.Close()
 
-    _, err = os.Stat(filepath.Join(rootDir, GO_DIR))
+    _, err = os.Stat(filepath.Join(basepath, GO_DIR))
     if os.IsNotExist(err) {
         ext := "tar.gz"
         if runtime.GOOS == "windows" { ext = "zip" }
@@ -54,10 +50,11 @@ func Init() (string, error) {
             return "", err
         }
 
-        if err := Uncompress(resp.Body, rootDir, runtime.GOOS); err != nil {
+        if err := Uncompress(resp.Body, basepath, runtime.GOOS); err != nil {
             return "", err
         }
     }
 
-    return rootDir, nil
+    return basepath, nil
 }
+
