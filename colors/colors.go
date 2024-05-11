c5421801ec3145cc8e19de40cf0785d0be8ed7ae
Kevin Suñer <ksuner@pm.me>
2024-05-11 19:56:48 +0200

    feat: added ansi colors

---
+++ colors/colors.go
@@ -0,0 +1,14 @@
+/*
+* SPDX-License-Identifier: GPL-3.0-only
+* Copyright (C) 2024 Kevin Suñer <ksuner@pm.me>
+ */
+
+package colors
+
+import "github.com/charmbracelet/lipgloss"
+
+const (
+    Background  = lipgloss.ANSIColor(237)
+    Gray        = lipgloss.ANSIColor(245)
+)
+
