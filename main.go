bdd6a0965db77b0a59afcf78ba2f91241ea59c0e
Kevin Suñer <ksuner@pm.me>
2024-05-16 00:16:12 +0200

    add tview, add flex layout with one column and two rows, add textarea

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/rivo/tview"
+)
+
 func main() {
-    println("Hello Gopa!")
+    app := tview.NewApplication()
+    
+    textarea := tview.NewTextArea().
+                SetWrap(false).
+                SetPlaceholder("Type some code here...")
+    textarea.SetTitle("Editor").SetBorder(true)
+    
+    flex := tview.NewFlex().
+            AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+                    AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+                            AddItem(textarea, 0, 1, false).
+                            AddItem(tview.NewBox().SetTitle("Console").SetBorder(true), 0, 1, false), 0, 1, false).
+                    AddItem(tview.NewBox().SetBorder(true), 5, 1, false), 0, 1, false)
+    if err := app.SetRoot(flex, true).EnableMouse(true).EnablePaste(true).Run(); err != nil {
+        panic(err)
+    }
 }
