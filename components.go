87dbcd641959f00f78498e829ceb1edea67ba038
Kevin Suñer <ksuner@pm.me>
2024-05-22 00:08:20 +0200

    working on the application styling

--- components.go
+++ components.go
@@ -18,7 +18,7 @@ type playground struct {
     layout          *tview.Flex
     editor          *tview.TextArea
     console         *tview.TextView
-    menu            *tview.Box
+    menu            *tview.TextView
     versionsList    *tview.List
 }
 
@@ -28,7 +28,7 @@ func newPlayground(app *tview.Application) playground {
         layout:         tview.NewFlex(),
         editor:         tview.NewTextArea(),
         console:        tview.NewTextView(),
-        menu:           tview.NewBox(),
+        menu:           tview.NewTextView(),
         versionsList:   tview.NewList(),
     }
 }
@@ -38,16 +38,33 @@ func (p playground) newLayout() *tview.Flex {
         AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
             AddItem(p.editor, 0, 1, false).
             AddItem(p.console, 0, 1, false), 0, 1, false).
-        AddItem(p.menu, 5, 1, false), 0, 1, false)
+        AddItem(p.menu, 3, 1, false), 0, 1, false)
 
     return p.layout
 }
 
 func (p playground) newEditor() *tview.TextArea {
+    p.editor.SetBackgroundColor(tcell.ColorDefault)
+    p.editor.SetBorderColor(tcell.Color246)
+    p.editor.SetTitleColor(tcell.Color246)
+    p.editor.SetPlaceholderStyle(
+        p.editor.GetPlaceholderStyle().Background(tcell.ColorDefault))
+    p.editor.SetPlaceholderStyle(
+        p.editor.GetPlaceholderStyle().Foreground(tcell.Color246))
+    p.editor.SetTextStyle(
+        p.editor.GetTextStyle().Background(tcell.ColorDefault))
     p.editor.SetWrap(false)
     p.editor.SetBorder(true)
     p.editor.SetTitle("Editor")
     p.editor.SetPlaceholder("Type some code here...")
+    p.editor.SetFocusFunc(func() {
+        p.editor.SetBorderColor(tcell.Color208)
+        p.editor.SetTitleColor(tcell.Color208)
+    })
+    p.editor.SetBlurFunc(func() {
+        p.editor.SetBorderColor(tcell.Color246)
+        p.editor.SetTitleColor(tcell.Color246)
+    })
     p.editor.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
         if event.Key() == tcell.KeyCtrlR {
             out, err := runCode(p.editor.GetText())
@@ -67,15 +84,30 @@ func (p playground) newEditor() *tview.TextArea {
 }
 
 func (p playground) newConsole() *tview.TextView {
+    p.console.SetBackgroundColor(tcell.ColorDefault)
+    p.console.SetBorderColor(tcell.Color246)
+    p.console.SetTitleColor(tcell.Color246)
     p.console.SetBorder(true)
     p.console.SetWordWrap(true)
     p.console.SetTitle("Console")
+    p.console.SetFocusFunc(func() {
+        p.console.SetBorderColor(tcell.Color208)
+        p.console.SetTitleColor(tcell.Color208)
+    })
+    p.console.SetBlurFunc(func() {
+        p.console.SetBorderColor(tcell.Color246)
+        p.console.SetTitleColor(tcell.Color246)
+    })
 
     return p.console
 }
 
-func (p playground) newMenu() *tview.Box {
+func (p playground) newMenu() *tview.TextView {
+    p.menu.SetBackgroundColor(tcell.ColorDefault)
+    p.menu.SetBorderColor(tcell.Color208)
+    p.menu.SetTextColor(tcell.Color208)
     p.menu.SetBorder(true)
+    p.menu.SetText("Run code (Ctrl-R) | Go version (Ctrl-L)")
 
     return p.menu
 }
